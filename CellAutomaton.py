238f9cb80a3236cd9d6fa7ad535f4d82cbce5591
Peter <peterw1114@gmail.com>
2019-04-11 23:42:06 +0200

    add random rules GUI

--- CellAutomaton.py
+++ CellAutomaton.py
@@ -5,23 +5,27 @@ class CellularAutomaton:
         self.number_of_ones = number_of_ones
 
         self.previous_state = []
-        self.current_state = self.get_initial_state()
+        self.current_state = []
+        self.set_initial_state()
 
         self.rule = rule
-        self.binary_rule = self.set_binary_rule()
+        self.binary_rule = []
+        self._set_binary_rule()
 
-    def calculate_next(self):
+        self.print_stats()
+
+    def calculate_next_iteration(self):
         self.previous_state = self.current_state
         self.current_state = []
         for cell_index in range(0, self.size):
             self._set_cell(cell_index)
 
-    def apply_rule(self, cell_index):
-        previous_triplet = self.get_previous_triplet(cell_index)
+    def _apply_rule(self, cell_index):
+        previous_triplet = self._get_previous_triplet(cell_index)
         rule_index = self.convert_from_binary_array_to_int(previous_triplet)
         return self.binary_rule[rule_index]
 
-    def get_previous_triplet(self, index):
+    def _get_previous_triplet(self, index):
         return [
             self.previous_state[(index - 1)],
             self.previous_state[index],
@@ -32,23 +36,22 @@ class CellularAutomaton:
     def convert_from_binary_array_to_int(binary_array):
         return int(''.join(str(e) for e in binary_array), 2)
 
-    def set_binary_rule(self):
-        return list(reversed(self.binary_array_from_int(self.rule)))
+    def _set_binary_rule(self):
+        self.binary_rule = list(reversed(self.binary_array_from_int(self.rule)))
 
     def binary_array_from_int(self, integer):
-        return [int(x) for x in list(self.binary8b_string_from_int(integer))]
+        return [int(x) for x in list(self._binary8b_string_from_int(integer))]
 
     @staticmethod
-    def binary8b_string_from_int(integer):
+    def _binary8b_string_from_int(integer):
         return '{0:08b}'.format(integer)
 
     def _set_cell(self, cell_index):
-        self.current_state.append(self.apply_rule(cell_index))
+        self.current_state.append(self._apply_rule(cell_index))
 
-    def get_initial_state(self):
-        if self.number_of_ones > int(self.size / 2):
-            self.number_of_ones = int(self.size / 2)
-        return [0] * int(self.size / 2) + [1] * self.number_of_ones + [0] * (int(self.size / 2) - self.number_of_ones)
+    def set_initial_state(self):
+        self.current_state = [0] * self.size
+        self.set_initial_ones()
 
     def get_current_state(self):
         return self.current_state
@@ -56,7 +59,22 @@ class CellularAutomaton:
     def print_rows(self, rows):
         print(self.current_state)
         for i in range(0, rows):
-            self.calculate_next()
+            self.calculate_next_iteration()
             print(self.current_state)
-        self.current_state = self.get_initial_state()
+        self.set_initial_state()
 
+    def print_stats(self):
+        print("Size: " + self.size.__str__())
+        print("Rule: " + self.rule.__str__())
+        print("Current state: " + self.current_state.__str__())
+
+    def set_initial_ones(self):
+        if self.number_of_ones > int(self.size / 2):
+            self.number_of_ones = int(self.size / 2)
+        middle = int(self.size/2)
+        for i in range(middle, middle+self.number_of_ones):
+            self.current_state[i] = 1
+
+    def set_rule(self, rule):
+        self.rule = rule
+        self._set_binary_rule()
--- app.py
+++ app.py
@@ -1,55 +1,84 @@
+from kivy.graphics.instructions import InstructionGroup
+import random
 from CellAutomaton import CellularAutomaton
 import kivy
 from kivy.config import Config
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Ellipse, Line
+from kivy.graphics import Color, Ellipse, Line, Rectangle
+import kivy.uix.button as kb
 kivy.require('1.9.0')
 
-class MyPaintWidget(Widget):
-    def __init__(self, ellipse_size,ellipse_offset):
-        super(MyPaintWidget, self).__init__()
-        Window.size = (1280, 800)
-        ellipse_offset = ellipse_offset
-        ellipse_size = ellipse_size
 
-        size = int(Window.size[0]/(ellipse_size+ellipse_offset))
-        iterations = int(Window.size[1]/(ellipse_size+ellipse_offset))
-        rule = 90
-        cell_automaton = CellularAutomaton(size, rule, number_of_ones=1)
-        cell_automaton.print_rows(iterations)
+class CellAutomatonWidget(Widget):
+    def __init__(self, ellipse_size, ellipse_offset, window_width=640, window_height=400, rule=90,):
+        super(CellAutomatonWidget, self).__init__()
+        Window.size = (window_width, window_height)
+        self.ellipse_offset = ellipse_offset
+        self.ellipse_size = ellipse_size
+        self.ellipse_box_size = self.ellipse_size + self.ellipse_offset
+        self.current_canvas_graphic_items = []
+        self.graphic_columns = int(Window.size[0]/self.ellipse_box_size)
+        self.graphic_rows = int(Window.size[1]/self.ellipse_box_size)
 
-        with self.canvas:
-            Color(1, 0, 0)
-            for i in range(0, iterations):
-                current_state = cell_automaton.get_current_state()
-                for e in range(0, size):
-                    if current_state[e] is 1:
-                        Ellipse(
-                            pos=(
-                                e*(ellipse_size+ellipse_offset),
-                                Window.size[1]-i*(ellipse_size+ellipse_offset)
-                            ),
-                            size=(
-                                ellipse_size,
-                                ellipse_size
-                            )
-                        )
-                cell_automaton.calculate_next()
+        self.cell_automaton = CellularAutomaton(size=self.graphic_columns, rule=rule, number_of_ones=1)
 
-    def on_touch_down(self, touch):
-        with self.canvas:
-            Color(1, 1, 0)
-            d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
+        self.set_initial_display()
 
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
+    def create_graphic(self):
+        self.canvas.add(Color(random.choice([0, 1]), random.choice([0, 1]), random.choice([0, 1])))
+        self._draw_graphic_rows()
+
+    def set_initial_display(self):
+        btn1 = kb.Button(text="Random Rule")
+        btn1.bind(on_press=self._draw_graphic_controller)
+        self.add_widget(btn1)
+
+    def _draw_graphic_controller(self, instance):
+        self.clear_graphic()
+        self.create_graphic()
+        self.cell_automaton.set_initial_state()
+        self.cell_automaton.set_rule((30+self.cell_automaton.rule) % 250)
+
+    def _draw_graphic_rows(self):
+        for row in range(0, self.graphic_rows):
+            self._draw_graphic_columns(row)
+            self.cell_automaton.calculate_next_iteration()
+
+    def _draw_graphic_columns(self, row):
+        current_state = self.cell_automaton.get_current_state()
+        for column in range(0, self.graphic_columns):
+            if current_state[column] is 1:
+                self._draw_graphic_cell(row, column)
+
+    def _draw_graphic_cell(self, row, column):
+        ellipse = Ellipse(
+                pos=(
+                    self._get_graphic_cell_x_pos(column),
+                    self._get_graphic_cell_y_pos(row)
+                ),
+                size=(
+                    self.ellipse_size,
+                    self.ellipse_size
+                )
+            )
+        self.canvas.add(ellipse)
+        self.current_canvas_graphic_items.append(ellipse)
+
+    def _get_graphic_cell_y_pos(self, row):
+        return Window.size[1] - (row + 1) * self.ellipse_box_size
+
+    def _get_graphic_cell_x_pos(self, column):
+        return column * self.ellipse_box_size
+
+    def clear_graphic(self):
+        for item in self.current_canvas_graphic_items:
+            self.canvas.remove(item)
+        self.current_canvas_graphic_items = []
 
 
-class MyPaintApp(App):
+class CellAutomatonApp(App):
     def build(self):
-        return MyPaintWidget(4,1)
+        return CellAutomatonWidget(4, 1, window_width=1000, window_height=600)
 

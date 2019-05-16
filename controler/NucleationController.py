57d2f1ed546dbdab998aa0dbdc87796d2b45a6b4
Peter <peterw1114@gmail.com>
2019-05-16 23:10:20 +0200

    Add change initial state mode radio inputs

--- controler/NucleationController.py
+++ controler/NucleationController.py
@@ -14,6 +14,8 @@ class NucleationController(Automaton2DController):
     def __init__(self, app, cell_size=9, cell_offset=1):
         super().__init__(app, cell_size, cell_offset)
         self.pattern_folder = "./patterns/Nucleation/"
+        self.app.view.sub_alive_cells.text = "-1%"
+        self.app.view.add_alive_cells.text = "+1%"
 
     def set_initial_view(self):
         self.set_view(NucleationView(self.modes, self.get_menu_width()))
@@ -22,6 +24,7 @@ class NucleationController(Automaton2DController):
         super().bind_buttons()
         self.bind_recrystallize_button()
         self.bind_periodic_checkbox()
+        self.bind_mode_checkboxes()
 
     def bind_recrystallize_button(self):
         self.app.view.recrystallize_button.bind(on_press=partial(self.recrystallize_button_controller))
@@ -64,13 +67,49 @@ class NucleationController(Automaton2DController):
         self.app.view.periodic_checkbox.bind(active=partial(self.toggle_periodic_controller))
 
     def toggle_periodic_controller(self, checkbox, value):
-        self.rule_set = NucleationRuleSet(
-            is_periodic=value,
-        )
+        self.rule_set.is_periodic = not self.rule_set.is_periodic
         self.set_cell_automaton(
             rule_set=self.rule_set
         )
         self.draw_current_state()
 
-    # def bind_rule_input(self):
-    #     self.app.view.mode_input.bind(on_text_validate=partial(self.rule_input_controller))
+    def bind_mode_checkboxes(self):
+        self.app.view.random_mode.bind(active=partial(self.random_mode_controller))
+        self.app.view.equal_mode.bind(active=partial(self.equal_mode_controller))
+        self.app.view.radius_mode.bind(active=partial(self.radius_mode_controller))
+
+    def random_mode_controller(self, instance, value):
+        if value is True:
+            self.change_rule_set_initial_mode("random")
+
+    def equal_mode_controller(self, instance, value):
+        if value is True:
+            self.change_rule_set_initial_mode("equal_spread")
+
+    def radius_mode_controller(self, instance, value):
+        if value is True:
+            self.change_rule_set_initial_mode("radius")
+
+    def change_rule_set_initial_mode(self, mode):
+        self.rule_set.initial_mode = mode
+        self.set_cell_automaton(
+            rule_set=self.rule_set
+        )
+        self.draw_current_state()
+        self.update_alive_cells_label()
+
+    def sub_alive_cells_controller(self, button_instance):
+        delta = -0.01
+        current_value = self.cell_automaton.get_percent_of_alive_cells()
+        if current_value + delta > 0:
+            self.cell_automaton.change_alive_cells_percentage(current_value + delta)
+            self.update_alive_cells_label()
+            self.draw_current_state()
+
+    def add_alive_cells_controller(self, button_instance):
+        delta = 0.01
+        current_value = self.cell_automaton.get_percent_of_alive_cells()
+        if current_value + delta <= 1:
+            self.cell_automaton.change_alive_cells_percentage(current_value + delta)
+            self.update_alive_cells_label()
+            self.draw_current_state()
